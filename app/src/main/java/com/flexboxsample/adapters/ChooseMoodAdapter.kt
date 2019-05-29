3ae550372459eec4c4bbca483fc7c99c5d702a81
Amardeep <Amardeepmanav1995@gmail.com>
2019-05-29 09:13:45 +0530

    SomeChanges(CleanUp)

--- app/src/main/java/com/flexboxsample/adapters/ChooseMoodAdapter.kt
+++ app/src/main/java/com/flexboxsample/adapters/ChooseMoodAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.RadioButton
 import androidx.recyclerview.widget.RecyclerView
 import com.flexboxsample.R
 import kotlinx.android.synthetic.main.choose_mood_item_layout.view.*
